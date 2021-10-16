e7eb183707f8092097a6d2c4396464aafedff9b3
Luca Spühler <spuehler.luca@gmail.com>
2021-10-16 13:24:56 +0200

    feat: dist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBv8-wcIXcLXDSEIUHJRPbipS8c-f9noWg',
+    authDomain: 'holys-bet-manager.firebaseapp.com',
+    databaseURL: 'https://holys-bet-manager-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'holys-bet-manager',
+    storageBucket: 'holys-bet-manager.appspot.com',
+    messagingSenderId: '730419012957',
+    appId: '1:730419012957:web:6d18e3d27aecd44dcf74b4',
+    measurementId: 'G-ZJYKXF4JJG',
+  },
 };
